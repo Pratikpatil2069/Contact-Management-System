7a205fd87e5c791719c220e73dbce63388958e7d
Pratik Patil <pratikpatil2069@gmail.com>
2026-08-20 00:09:08 +0530

    Handle Http status codes using ResponseEntity

--- Backend/ContactManagementSystem/src/main/java/ContactManagementSystem/ContactController/ContactController.java
+++ Backend/ContactManagementSystem/src/main/java/ContactManagementSystem/ContactController/ContactController.java
@@ -3,6 +3,8 @@ package ContactManagementSystem.ContactController;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,28 +29,33 @@ public class ContactController {
 	private ContactServices contactServices;
 	
 	@PostMapping("/addContact")
-	public ContactModel addContact(@Valid @RequestBody ContactModel contactModel) {
-		return contactServices.addContact(contactModel);
+	public ResponseEntity<ContactModel> addContact(@Valid @RequestBody ContactModel contactModel) {
+		ContactModel contact= contactServices.addContact(contactModel);
+		return ResponseEntity.status(HttpStatus.CREATED).body(contact);
 	}
 	
 	@PutMapping("/updateContact/{id}")
-	public ContactModel updateContact(@PathVariable  String id,@Valid @RequestBody ContactModel contactModel) {
-		return contactServices.updateContact(id, contactModel);
+	public ResponseEntity<ContactModel> updateContact(@PathVariable  String id,@Valid @RequestBody ContactModel contactModel) {
+		ContactModel contact= contactServices.updateContact(id, contactModel);
+		return ResponseEntity.ok(contact);
 	}
 	
 	@DeleteMapping("/deleteContact/{id}")
-	public ContactModel deleteContact(@PathVariable String id) {
-		return contactServices.deleteContact(id);
+	public ResponseEntity<Void> deleteContact(@PathVariable String id) {
+		 contactServices.deleteContact(id);
+		 return ResponseEntity.noContent().build();
 	}
 	
 	@GetMapping("/getAllContacts")
-	public List<ContactModel> getAllContact(){
-		return contactServices.getAllContacts();
+	public ResponseEntity<List<ContactModel>> getAllContact(){
+		List<ContactModel> list= contactServices.getAllContacts();
+		return ResponseEntity.ok(list);
 	}
 	
 	@GetMapping("/getContactById/{id}")
-	public ContactModel getContactById(@PathVariable String id) {
-		return contactServices.getContactById(id);
+	public ResponseEntity<ContactModel> getContactById(@PathVariable String id) {
+		ContactModel contact= contactServices.getContactById(id);
+		return ResponseEntity.ok(contact);
 	}
 	
 	
